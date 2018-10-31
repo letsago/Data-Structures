8aca9d5e41bf2753f33e6324e4ecfeb0687eabba
Patrick Yu <pa.yu@hotmail.com>
2018-10-31 04:33:25 +0000

    Merged PR 118: graph test cases + header
    graph test cases + header
    
    Related work items: #145

---
+++ Practice/Graph/graph_test.cpp
@@ -0,0 +1,231 @@
+#include <gtest/gtest.h>
+#include <pyu/graph.h>
+#include <pyu/common.h>
+#include <pyu/vector.h>
+
+using namespace pyu;
+
+template<class T>
+class GraphTests : public ::testing::Test {};
+
+struct ComplexType
+{
+    ComplexType() {};
+
+    ComplexType(int val) : m_value(val) {};
+
+    bool operator> (const ComplexType& other) const
+    {
+        return m_value > other.m_value;
+    }
+
+    bool operator== (const ComplexType& other) const
+    {
+        return m_value == other.m_value;
+    }
+
+    int m_value;
+};
+
+template<typename T>
+Vector<T> DataGenerator(const int arr[], size_t size)
+{
+    return Vector<T>();
+}
+
+template<>
+Vector<int> DataGenerator<int>(const int arr[], size_t size)
+{
+    Vector<int> x;
+
+    for (uint32_t i = 0; i < size; ++i)
+        x.insert_back(arr[i]);
+
+    return x;
+}
+
+template<>
+Vector<ComplexType> DataGenerator<ComplexType>(const int arr[], size_t size)
+{
+    Vector<ComplexType> x;
+
+    for (uint32_t i = 0; i < size; ++i)
+    {
+        ComplexType value(arr[i]);
+        x.insert_back(value);
+    }
+
+    return x;
+}
+
+typedef testing::Types<int, ComplexType> Implementations;
+TYPED_TEST_CASE(GraphTests, Implementations);
+
+TYPED_TEST(GraphTests, DistinctConnectTest)
+{
+    const int arr[] = {10, 5, 20, 15};
+    Vector<TypeParam> data = DataGenerator<TypeParam>(arr, ARRAYSIZE(arr));
+    Graph<TypeParam> A;
+    ASSERT_TRUE(A.empty());
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.front(), data.at(i));
+        ASSERT_EQ(A.size(), i + 1);
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+}
+
+TYPED_TEST(GraphTests, NonDistinctConnectTest)
+{
+    const int arr[] = {10, 5, 20, 15};
+    Vector<TypeParam> data = DataGenerator<TypeParam>(arr, ARRAYSIZE(arr));
+    Graph<TypeParam> A;
+    ASSERT_TRUE(A.empty());
+    A.connect(data.front(), data.front());
+    ASSERT_TRUE(A.contains(data.front()));
+    ASSERT_EQ(A.size(), 1);
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.front(), data.at(i));
+        ASSERT_EQ(A.size(), i + 1);
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.at(i), data.front());
+        ASSERT_EQ(A.size(), data.length());
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+}
+
+TYPED_TEST(GraphTests, RemoveTest)
+{
+    const int arr[] = {10, 5, 20, 15};
+    Vector<TypeParam> data = DataGenerator<TypeParam>(arr, ARRAYSIZE(arr));
+    Graph<TypeParam> A;
+    ASSERT_TRUE(A.empty());
+    ASSERT_FALSE(A.remove(data.front()));
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.front(), data.at(i));
+        ASSERT_EQ(A.size(), i + 1);
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+    {
+        ASSERT_TRUE(A.remove(data.at(i)));
+        ASSERT_FALSE(A.remove(data.at(i)));
+        ASSERT_EQ(A.size(), data.length() - i - 1);
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_FALSE(A.contains(data.at(i)));
+
+    ASSERT_TRUE(A.empty());
+}
+
+TYPED_TEST(GraphTests, ShortestDistanceConnectedTest)
+{
+    const int arr[] = {10, 5, 20, 15};
+    Vector<TypeParam> data = DataGenerator<TypeParam>(arr, ARRAYSIZE(arr));
+    Graph<TypeParam> A;
+    ASSERT_TRUE(A.empty());
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.front(), data.at(i));
+        ASSERT_EQ(A.size(), i + 1);
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+    ASSERT_EQ(A.shortestDistance(data.front(), data.front()), 0);
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+        ASSERT_EQ(A.shortestDistance(data.front(), data.at(i)), 1);
+
+    for (uint32_t i = 2; i < data.length(); ++i)
+        ASSERT_EQ(A.shortestDistance(data.at(1), data.at(i)), 2);
+
+    A.connect(data.at(1), data.at(2));
+    ASSERT_EQ(A.shortestDistance(data.at(1), data.at(2)), 1);
+}
+
+TYPED_TEST(GraphTests, DistinctShortestDistanceNonConnectedTest)
+{
+    const int arr[] = {10, 5, 20, 15};
+    Vector<TypeParam> data = DataGenerator<TypeParam>(arr, ARRAYSIZE(arr));
+    Graph<TypeParam> A;
+    ASSERT_TRUE(A.empty());
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.front(), data.at(i));
+        ASSERT_EQ(A.size(), i + 1);
+    }
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+        ASSERT_EQ(A.shortestDistance(data.front(), data.at(i)), 1);
+
+    ASSERT_TRUE(A.remove(data.front()));
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+        ASSERT_THROW(A.shortestDistance(data.front(), data.at(i)), std::out_of_range);
+}
+
+TYPED_TEST(GraphTests, ContainsTest)
+{
+    const int arr[] = {10, 5, 20, 15};
+    Vector<TypeParam> data = DataGenerator<TypeParam>(arr, ARRAYSIZE(arr));
+    const int arr2[] = {80};
+    Vector<TypeParam> nonContainedData = DataGenerator<TypeParam>(arr2, ARRAYSIZE(arr2));
+    Graph<TypeParam> A;
+    ASSERT_TRUE(A.empty());
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_FALSE(A.contains(data.at(i)));
+
+    ASSERT_TRUE(A.empty());
+
+    for (uint32_t i = 1; i < data.length(); ++i)
+    {
+        A.connect(data.front(), data.at(i));
+        ASSERT_EQ(A.size(), i + 1);
+    }
+
+    for (uint32_t i = 0; i < nonContainedData.length(); ++i)
+        ASSERT_FALSE(A.contains(nonContainedData.at(i)));
+
+    for (uint32_t i = 0; i < data.length(); ++i)
+        ASSERT_TRUE(A.contains(data.at(i)));
+
+    ASSERT_EQ(A.size(), data.length());
+}
---
+++ Practice/pyu_libs/pyu/graph.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <stdexcept>
+
+namespace pyu
+{
+template <typename T>
+class Graph
+{
+public:
+    void connect(const T& A, const T& B)
+    {
+
+    }
+
+    // throws exception if A and B are not connected some way
+    uint32_t shortestDistance(const T& A, const T& B) const
+    {
+        throw std::out_of_range("No connection exists");
+    }
+
+    bool remove(const T& A)
+    {
+        return false;
+    }
+
+    bool contains(const T& A) const
+    {
+        return false;
+    }
+
+    size_t size() const
+    {
+        return m_size;
+    }
+
+    bool empty() const
+    {
+        return (size() == 0);
+    }
+
+private:
+    size_t m_size;
+};
+
+}
